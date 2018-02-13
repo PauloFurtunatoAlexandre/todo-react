f96664ddab81a471fe4d57bb751617f1ddfd4088
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2018-02-12 19:35:54 -0700

    correct the import of the components into app.jsx

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,8 +2,9 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from 'todo'
-import about from 'about'
+import Todo from '../todo/todo'
+import about from '../about/about'
+import Menu from '../template/menu'
 
 export default props => (
     <div className='container'>
