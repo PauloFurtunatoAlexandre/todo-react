5152c28e30ed419fd5e9293ca4bdedcfd18f8b31
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2018-02-13 19:00:41 -0700

    Form and List created inside Todo

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageheader'
 
 export default props => (
-    <div>
-        <h1>About</h1>
-    </div>
-)
+  <div>
+    <PageHeader name="About" small="Who we are" />
+
+    <h2>Our story</h2>
+    <p>Lorem Ipsum dolor sit amet...</p>
+    <h2>Mission</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Press</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+  </div>
+);
--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 
 export default props => (
-  <nav className="navbar navbar-inverse bg-inverse">
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container">
       <a className="navbar-brand" href="#">
-        <i className="fa fa-calendar-check-o" /> TodoApp
+        <i className="far fa-calendar-alt" /> TodoApp
       </a>
     </div>
 
     <div id="navbar" className="navbar-collapse collapse">
       <ul className="nav navbar-nav">
         <li>
-          <a href="#/todos">Tarefas</a>
+          <a href="#/todos">Tasks</a>
         </li>
         <li>
-          <a href="#/about">Sobre</a>
+          <a href="#/about">About</a>
         </li>
       </ul>
     </div>
-
   </nav>
 );
